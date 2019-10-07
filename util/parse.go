7c4ea0ce3460cb5f175a2e0eea942ee617767995
HikaruEgashira <shighten@gmail.com>
2019-10-07 18:42:40 +0900

    feat: can scraping github.com

--- util/parse.go
+++ util/parse.go
@@ -1,35 +1,29 @@
-package util
+package main
 
 import (
 	"fmt"
-	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func Parse() ([]string, error) {
-	doc, _ := goquery.NewDocument("http://favstar.fm/users/mattn_jp/")
-	doc.Find(".fs-tweet").Each(func(_ int, s *goquery.Selection) {
-		fmt.Println(s.Find(".fs-tweet-text").Text())
+	urls := []string{}
 
-		s.Find("div[data-type='favs']").Each(func(_ int, s *goquery.Selection) {
-			fmt.Printf("FAV(%s): %s ...\n",
-				s.Find("li.fs-total").Text(),
-				strings.Join(s.Find("a").Map(func(_ int, s *goquery.Selection) string {
-					a, _ := s.Attr("title")
-					return a
-				}), ", "))
-		})
-		s.Find("div[data-type='retweets']").Each(func(_ int, s *goquery.Selection) {
-			fmt.Printf("RT (%s): %s ...\n",
-				s.Find("li.fs-total").Text(),
-				strings.Join(s.Find("a").Map(func(_ int, s *goquery.Selection) string {
-					a, _ := s.Attr("title")
-					return a
-				}), ", "))
-		})
+	doc, err := goquery.NewDocument("https://github.com/trending")
+	if err != nil {
+		fmt.Print("error: url scrapping failed")
+	}
 
-		fmt.Println()
+	doc.Find(".Box-row .lh-condensed").Each(func(_ int, s *goquery.Selection) {
+		s.Find("a").Each(func(_ int, a *goquery.Selection) {
+			url, _ := a.Attr("href")
+			urls = append(urls, url)
+		})
 	})
-	return nil, nil
+	return urls, nil
+}
+
+func main() {
+	urls, _ := Parse()
+	fmt.Println(urls)
 }
